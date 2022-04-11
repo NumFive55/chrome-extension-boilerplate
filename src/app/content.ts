5c136d468c9563090e3c69af5203017ac5bea653
NumFive55 <37964923+NumFive55@users.noreply.github.com>
2022-04-11 08:54:14 -0400

    First commit

--- src/app/content.ts
+++ src/app/content.ts
@@ -1,8 +1,45 @@
-chrome.runtime.sendMessage({}, (response) => {
-    var checkReady = setInterval(() => {
-        if (document.readyState === "complete") {
-            clearInterval(checkReady)
-            console.log("We're in the injected content script!")
-        }
-    })
-})
+// chrome.runtime.sendMessage({}, (response) => {
+//   var checkReady = setInterval(() => {
+//     if (document.readyState === "complete") {
+//       clearInterval(checkReady);
+//       console.log("We're in the injected content script!");
+//     }
+//   });
+// });
+
+const features = [];
+const activeFeatures = [];
+
+chrome.storage.sync.get("featureList", (result) => {
+  const featureObject = result.featureList;
+  featureObject.forEach((element) => {
+    features.push({ id: element.id, on: element.checked });
+    if (element.checked) {
+      activeFeatures.push(element.name);
+    }
+  });
+});
+
+chrome.storage.onChanged.addListener(function (changes, area) {
+  console.log(changes);
+  console.log(area);
+});
+
+runFeatures(activeFeatures);
+
+console.log(features);
+console.log(activeFeatures);
+
+function runFeatures(pref) {
+  console.log("run features ran");
+  if (pref.includes("testFeature")) testFeature();
+  if (pref.includes("testFeatureTwo")) testFeature();
+}
+
+function testFeature() {
+  console.log("test feature one");
+}
+
+function testFeatureTwo() {
+  console.log("test feature two");
+}
--- src/ui/popup.tsx
+++ src/ui/popup.tsx
@@ -1,21 +1,81 @@
-import * as React from "react"
-import * as ReactDOM from "react-dom"
+import * as React from "react";
+import { useState, useCallback, useEffect } from "react";
+import * as ReactDOM from "react-dom";
 
-import "../styles/popup.css"
+import "../styles/popup.css";
 
-class Hello extends React.Component {
-    render() {
-        return (
-            <div className="popup-padded">
-                <h1>{ chrome.i18n.getMessage("l10nHello") }</h1>
-            </div>
-        )
-    }
-}
+const Popup = () => {
+  // SET STATE FOR ACTIVE FEATURES IN STATE SETTER
+  // USEEFFECT ON UPDATE OF STATE AND STORE STATE IN LOCAL STORAGE
+  // ACCESS LOCAL STORAGE IN CONTENT.TS
+
+  interface Feature {
+    name: string;
+    id: string;
+    checked: boolean;
+  }
+
+  const allFeatures: Feature[] = [
+    { name: "Test Feature", id: "testFeature", checked: false },
+    { name: "Test Feature Two", id: "testFeatureTwo", checked: false },
+  ];
+
+  const [features, setFeatures] = useState(allFeatures);
+
+  useEffect(() => {
+    chrome.storage.sync.get("featureList", (result) => {
+      setFeatures(result.featureList);
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log("feature changed");
+    chrome.storage.sync.set({ featureList: features }, () => {});
+  }),
+    [features];
+
+  const featureSelected = useCallback(
+    (e) => {
+      // Finds index of feature selected
+      const featureIndex = features.findIndex(({ id }) => id === e.target.id);
+
+      // Sets state of feature in object
+      let featuresList = [...features];
+      let updatedFeature = features[featureIndex];
+
+      // Checks state of checkbox
+      if (updatedFeature.checked) {
+        updatedFeature.checked = false;
+      } else {
+        updatedFeature.checked = true;
+      }
+
+      // Updates shallow feature list with updated feature
+      featuresList[featureIndex] = updatedFeature;
+
+      // Sets new features
+      setFeatures(featuresList);
+    },
+    [features]
+  );
+
+  return (
+    <div className="popup-padded">
+      {features.map((feature, index) => (
+        <div key={index}>
+          <h1>{feature.name}</h1>
+          <input
+            type="checkbox"
+            id={feature.id}
+            onChange={featureSelected}
+            checked={feature.checked}
+          ></input>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 // --------------
 
-ReactDOM.render(
-    <Hello />,
-    document.getElementById('root')
-)
+ReactDOM.render(<Popup />, document.getElementById("root"));
